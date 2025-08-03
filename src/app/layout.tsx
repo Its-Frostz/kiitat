cf439582033bfc75b2e69a70b3969fe1a031a944
Atif Ahmed <thecoldqrage@gmail.com>
2025-08-03 15:26:58 +0530

    Added a new Backgroud

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import FlowyCloth from "@/components/VaporWave";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div style={{ width: '100vw', height: '100vh', position: 'fixed', zIndex: "-99" }}>
+          <FlowyCloth />
+        </div>
         {children}
       </body>
     </html>
